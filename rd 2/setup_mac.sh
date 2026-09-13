c8c35fa6a3e530df261900eb1a8f0565189b1523
Sarthak Dewangan <72980936+sarthak05012006@users.noreply.github.com>
2026-09-13 10:44:11 +0530

    Delete rd 2 directory

--- rd 2/setup_mac.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd "$(dirname "$0")"
-python3 -m venv .venv
-source .venv/bin/activate
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-echo "Setup complete. Next: python3 training/prepare_dataset.py"
